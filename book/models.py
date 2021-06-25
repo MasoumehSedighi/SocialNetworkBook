4e6300c46f2e16f672b67946e9b4fcb0b78276f9
MasoumehSedighi <mas_cutt4@yahoo.com>
2021-06-25 18:00:06 +0430

    second commit

--- book/models.py
+++ book/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField('name', max_length=30)
+
+
+class Book(models.Model):
+    class Meta:
+        verbose_name = 'کتاب'
+        verbose_name_plural = 'کتابها'
+    STATUS = [('F', 'free'), ('B', 'borrowed'), ('D', 'deprecated')]
+    name = models.CharField('book_name', max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publish_year = models.DateTimeField('year of publication')
+    record_Date = models.DateTimeField('Time to record books', auto_now_add=True, null=True)
+    update_time = models.DateTimeField('update_time', auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS)
+
+    @property
+    def full_name(self):
+        return f'{self.name},{self.author}'
+
+    def __str__(self):
+        return f'{self.name} and {self.update_time}'
+
+    def change_status(self):
+        if self.status == 'F':
+            self.status = 'B'
+        else:
+            self.status = 'F'
+        self.save()
+        return self.status
+
+    def get_publish_year(self):
+        return self.publish_year.year
--- user/models.py
+++ user/models.py
@@ -2,15 +2,33 @@ from django.db import models
 
 
 class User(models.Model):
+    GENDER_CHOICES = [('F', 'female'), ('M', 'male')]
     first_name = models.CharField('first name', max_length=100, null=True)
-    last_name = models.CharField('last name', max_length=100, null=True, blank=True)
-    username = models.CharField('username', max_length=10,null=False)
-    profile = models.TextField('description',max_length=150)
-    gender = models.Choices('gender')
-    phone_number = models.CharField('phone number', max_length=11,blank=True)
-    biography = models.CharField('biography', null=True)
-    country = models.CharField('country', null=True)
+    last_name = models.CharField('last name', max_length=130, null=True, blank=True)
+    username = models.CharField('username', max_length=50, null=True)
+    profile = models.TextField('description', max_length=150)
+    gender = models.CharField('gender', max_length=1, choices=GENDER_CHOICES, default='F')
+    phone_number = models.CharField('phone number', max_length=11, blank=True)
+    biography = models.CharField('biography', max_length=100, null=True)
+    country = models.CharField('country', max_length=20, null=True)
     website = models.URLField('website')
     email = models.EmailField('email')
-    register_date = models.DateTimeField('register date',auto_now_add=True)
-    update_date = models.DateTimeField('update date')
+    register_date = models.DateTimeField('register date', auto_now_add=True)
+    update_date = models.DateTimeField('update date', auto_now_add=True)
+    credit=models.IntegerField('credit', default=20)
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return f'{self.username} register on {self.register_date}'
+
+    def delete(self):
+        delete_obj = f'{self.first_name}{self.last_name} deleted on'
+        self.delete()
+        return delete_obj
+
+    def update_credit(self, amount):
+        self.credit += amount
+        self.save()
